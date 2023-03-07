8fa6c86d2a06e8531ef663692cdd5906c97b098e
andreybyrsa <byrsa2004@bk.ru>
2023-03-07 08:36:02 +0500

    Исправил ошибку с цветом у Typography

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,6 +1,17 @@
+import Typography from '@Components/Typography'
+
 import './App.scss'
 
 function App() {
-  return <div className="App">123</div>
+  return (
+    <div className="App">
+      <Typography
+        variant="title-t2-semiBold"
+        color="color-text-tertiary"
+      >
+        Typography
+      </Typography>
+    </div>
+  )
 }
 export default App
