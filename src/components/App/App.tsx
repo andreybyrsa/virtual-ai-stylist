e27b2ec01a1d542cae2c9c8798d84cc181bd4268
kivaX9 <kirill.vlasov.05@inbox.tu>
2023-03-07 18:16:12 +0500

    Недоделанный Button

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,6 +1,12 @@
+import Button from '@Components/Button'
+
 import './App.scss'
 
 function App() {
-  return <div className="App">123</div>
+  return (
+    <div>
+      <Button />
+    </div>
+  )
 }
 export default App
