5e4c5bdb4cf00da564559bdee0fb5362512da9f9
andreybyrsa <byrsa2004@bk.ru>
2023-03-07 19:08:03 +0500

    Сделал компонент обертки страницы PageLayout

---
+++ src/layouts/PageLayout/PageLayout.tsx
@@ -0,0 +1,29 @@
+import classNames from 'classnames'
+
+import './PageLayout.scss'
+import PageLayoutProps from './PageLayout.types'
+
+function PageLayout({
+  className,
+  headerClassName,
+  contentClassName,
+  footerClassName,
+
+  header,
+  children,
+  footer,
+}: PageLayoutProps) {
+  const PageLayoutClassName = classNames('page-layout', className)
+  const PageLayoutHeaderClassName = classNames('page-layout__header', headerClassName)
+  const PageLayoutContentClassName = classNames('page-layout__content', contentClassName)
+  const PageLayoutFooterClassName = classNames('page-layout__footer', footerClassName)
+  return (
+    <div className={PageLayoutClassName}>
+      <div className={PageLayoutHeaderClassName}>{header}</div>
+      <div className={PageLayoutContentClassName}>{children}</div>
+      <div className={PageLayoutFooterClassName}>{footer}</div>
+    </div>
+  )
+}
+
+export default PageLayout
