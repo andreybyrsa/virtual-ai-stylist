6722ea16eb92ea66277ccfd9305d30c1caf52d65
andreybyrsa <byrsa2004@bk.ru>
2023-03-08 11:08:49 +0500

    Добавил новые иконки

--- src/pages/DevPage/DevPage.tsx
+++ src/pages/DevPage/DevPage.tsx
@@ -9,9 +9,10 @@ function DevPage() {
     <PageLayout contentClassName="dev-page">
       <Typography variant="title-t2-semiBold">Dev Page</Typography>
       <Icon
-        iconName="check"
+        iconName="house"
         color="color-icon-correct"
         size={40}
+        viewBox={45}
       />
       <Button type="primary" />
       <Button type="secondary" />
