5fae7f50523b7c84fec3a15e1766297581e8d783
kivaX9 <kirill.vlasov.05@inbox.tu>
2023-03-13 21:35:36 +0500

    Сделал страницу Favourites

---
+++ src/pages/FavouritesPage/FavouritesPage.tsx
@@ -0,0 +1,69 @@
+import { useMemo } from 'react'
+
+import Card from '@Components/Cards/Card'
+import Icon from '@Components/Icon'
+import NavigationSideBar from '@Components/NavigationComponents/NavigationSideBar'
+import Typography from '@Components/Typography'
+
+import { jacket } from '@Assets/Images'
+
+import Footer from '@Layouts/Footer'
+import Header from '@Layouts/Header'
+import PageLayout from '@Layouts/PageLayout'
+
+import './FavouritesPage.scss'
+
+const cards = [
+  { id: 0, name: 'card0', src: [jacket, jacket, jacket], date: Date.now().toString() },
+  { id: 1, name: 'card1', src: [jacket, jacket, jacket], date: Date.now().toString() },
+  { id: 2, name: 'card2', src: [jacket, jacket, jacket], date: Date.now().toString() },
+  { id: 3, name: 'card3', src: [jacket, jacket, jacket], date: Date.now().toString() },
+  { id: 4, name: 'card4', src: [jacket, jacket, jacket], date: Date.now().toString() },
+]
+
+function FavouritesPage() {
+  const header = useMemo(() => {
+    return (
+      <Header
+        rightSideSlot={<Typography variant="title-t1-bold">Избранное</Typography>}
+        leftSideSlot={
+          <Icon
+            iconName="arrowBack"
+            color="color-icon-secondary"
+            viewBox={35}
+            size={35}
+          />
+        }
+      />
+    )
+  }, [])
+
+  const footer = useMemo(() => {
+    return (
+      <Footer isNavigation>
+        <NavigationSideBar />
+      </Footer>
+    )
+  }, [])
+
+  return (
+    <PageLayout
+      contentClassName="favourites-page__content"
+      header={header}
+      footer={footer}
+    >
+      <div className="favourites-page__content-cards">
+        {cards.map((elem) => (
+          <Card
+            key={elem.id}
+            cardName={elem.name}
+            imageSrces={elem.src}
+            date={elem.date}
+          />
+        ))}
+      </div>
+    </PageLayout>
+  )
+}
+
+export default FavouritesPage
--- src/router/index.tsx
+++ src/router/index.tsx
@@ -1,10 +1,15 @@
 import { createBrowserRouter } from 'react-router-dom'
 
 import DevPage from '@Pages/DevPage'
+import FavouritesPage from '@Pages/FavouritesPage'
 import IndexPage from '@Pages/IndexPage'
 import OnBoardingPage from '@Pages/OnBoardingPage'
 
 const router = createBrowserRouter([
+  {
+    path: '/favourites',
+    element: <FavouritesPage />,
+  },
   {
     path: '/',
     element: <IndexPage />,
