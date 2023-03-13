c7942e2738e115fd14020d52a5179eda34cfa57f
andreybyrsa <byrsa2004@bk.ru>
2023-03-13 20:47:36 +0500

    Сверстал страницу IndexPage

--- src/router/index.tsx
+++ src/router/index.tsx
@@ -1,16 +1,21 @@
 import { createBrowserRouter } from 'react-router-dom'
 
 import DevPage from '@Pages/DevPage'
+import IndexPage from '@Pages/IndexPage'
 import OnBoardingPage from '@Pages/OnBoardingPage'
 
 const router = createBrowserRouter([
   {
     path: '/',
-    element: <DevPage />,
+    element: <IndexPage />,
   },
   {
     path: '/on-boarding',
     element: <OnBoardingPage />,
+  },
+  {
+    path: '/dev',
+    element: <DevPage />,
   },
 ])
 
