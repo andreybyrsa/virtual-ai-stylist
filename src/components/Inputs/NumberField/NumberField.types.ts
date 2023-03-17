b0ed8561ec9f66a10f39f3ba7d3034c685d5611d
andreybyrsa <byrsa2004@bk.ru>
2023-03-17 17:51:59 +0500

    Добавил возможность выводить value из компонента NumberField

--- src/components/Inputs/NumberField/NumberField.types.ts
+++ src/components/Inputs/NumberField/NumberField.types.ts
@@ -1,7 +1,9 @@
-import { InputHTMLAttributes } from 'react'
+import React, { InputHTMLAttributes, SetStateAction } from 'react'
 
 interface NumberFieldProps extends InputHTMLAttributes<HTMLInputElement> {
   label?: string
+
+  setValue?: React.Dispatch<SetStateAction<string>>
 }
 
 export default NumberFieldProps
