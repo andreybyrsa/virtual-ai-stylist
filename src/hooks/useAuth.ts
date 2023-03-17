d321bfb0f0e49e28a32044b7ebfcdf22dc052374
andreybyrsa <byrsa2004@bk.ru>
2023-03-17 18:35:12 +0500

    Поправил селектор для User

--- src/hooks/useAuth.ts
+++ src/hooks/useAuth.ts
@@ -2,7 +2,7 @@ import userSelector from '@Store/reducers/user/UserSelector'
 
 const useAuth = () => {
   const user = userSelector()
-  return !!user.user?.id
+  return !!user?.id
 }
 
 export default useAuth
--- src/store/reducers/user/UserSelector.ts
+++ src/store/reducers/user/UserSelector.ts
@@ -3,7 +3,7 @@ import { useSelector } from 'react-redux'
 import { RootState } from '@Store/index'
 
 function UserSelector() {
-  return useSelector((state: RootState) => state.user)
+  return useSelector((state: RootState) => state.user.user)
 }
 
 export default UserSelector
