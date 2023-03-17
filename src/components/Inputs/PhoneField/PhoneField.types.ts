274bf6f78117a1f98e4dfa3075a8be694e3be199
andreybyrsa <byrsa2004@bk.ru>
2023-03-17 17:58:13 +0500

    Добавил возможность выводить value из компонента PhoneField

--- src/components/Inputs/PhoneField/PhoneField.types.ts
+++ src/components/Inputs/PhoneField/PhoneField.types.ts
@@ -1,7 +1,9 @@
-import { InputHTMLAttributes } from 'react'
+import React, { InputHTMLAttributes, SetStateAction } from 'react'
 
 interface PhoneFieldProps extends InputHTMLAttributes<HTMLInputElement> {
   label?: string
+
+  setCurrentValue?: React.Dispatch<SetStateAction<string>>
 }
 
 export default PhoneFieldProps
